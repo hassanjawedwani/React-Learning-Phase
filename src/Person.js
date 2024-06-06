0b38a419e2a0f04c8c9be710ced0cdcb04637630
firstRepo <hassanjawedwani@gmail.com>
2024-06-07 04:12:18 +0500

    start

---
+++ src/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Person extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <h1>Hello Person</h1>
+        );
+    }
+};
+
+export default Person;
